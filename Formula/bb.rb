91d6bb1b043644a4b4b602e39ece5107cb02b38d
woodpecker <ci@wiebe.xyz>
2026-07-07 13:32:50 +0200

    Update formula to 0.236.123

--- Formula/bb.rb
+++ Formula/bb.rb
@@ -1,16 +1,16 @@
 class Bb < Formula
   desc "BugBarn"
   homepage "https://github.com/wiebe-xyz/bugbarn"
-  version "0.236.122"
+  version "0.236.123"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://webwiebe.nl/brew/bb-darwin-amd64-0.236.122.tar.gz"
-      sha256 "7d3ae08ad153a71925626cc6a336a93d87c690be1c621e1e16b9ef4070b9037f"
+      url "https://webwiebe.nl/brew/bb-darwin-amd64-0.236.123.tar.gz"
+      sha256 "ecf73e08847214b4893019b1180ccb30d6ecac9fa182944d8d8c8173aaaa8736"
     elsif Hardware::CPU.arm?
-      url "https://webwiebe.nl/brew/bb-darwin-arm64-0.236.122.tar.gz"
-      sha256 "786b3da566920f0f6e56f7a3664675cc064b1bba1d2cc6b8924086d83c65ed7f"
+      url "https://webwiebe.nl/brew/bb-darwin-arm64-0.236.123.tar.gz"
+      sha256 "952cccc722eeaeb5684e87fddbcbba5db12b92594d59d7e061becb3a68a813f0"
     end
   end
 
--- Formula/bugbarn.rb
+++ Formula/bugbarn.rb
@@ -1,16 +1,16 @@
 class Bugbarn < Formula
   desc "BugBarn"
   homepage "https://github.com/wiebe-xyz/bugbarn"
-  version "0.236.122"
+  version "0.236.123"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://webwiebe.nl/brew/bugbarn-darwin-amd64-0.236.122.tar.gz"
-      sha256 "273025a66094a5df4c9ac1fe91078d39ef545034567fc225744c40cfe8aebabf"
+      url "https://webwiebe.nl/brew/bugbarn-darwin-amd64-0.236.123.tar.gz"
+      sha256 "7af38941c932611dafccee2d7003019896d3f5c526da7e1beadaf282b5554d90"
     elsif Hardware::CPU.arm?
-      url "https://webwiebe.nl/brew/bugbarn-darwin-arm64-0.236.122.tar.gz"
-      sha256 "57907f85d97f850d3c0301d8b73001ed850731960150101caa3bd0b180deebb0"
+      url "https://webwiebe.nl/brew/bugbarn-darwin-arm64-0.236.123.tar.gz"
+      sha256 "11ad0696197285d9a5662a872fb61b223181973b503325de0c975852a09cb6ed"
     end
   end
 
